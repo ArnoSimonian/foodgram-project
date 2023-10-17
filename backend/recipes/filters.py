8cf7e9378204f1d4f841401b1c18fd5c2b51e71c
Givi Lobstel Anasis <lobstel@ASheina-h.local>
2023-10-17 05:10:43 +0300

    Изменения в filters.py

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -28,20 +28,16 @@ class RecipeFilter(django_filters.FilterSet):
 
     def filter_is_favorited(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
-            recipe_ids = set(
-                self.request.user.favorited_by.values_list(
+            recipe_ids = self.request.user.favorited_by.values_list(
                     'recipe_id', flat=True
                 )
-            )
-            return queryset.filter(id__in=recipe_ids)
+            return set(queryset.filter(id__in=recipe_ids))
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
-            recipe_ids = set(
-                self.request.user.in_shopping_cart_of.values_list(
+            recipe_ids = self.request.user.in_shopping_cart_of.values_list(
                     'recipe_id', flat=True
                 )
-            )
-            return queryset.filter(id__in=recipe_ids)
+            return set(queryset.filter(id__in=recipe_ids))
         return queryset
